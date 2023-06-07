0be1ede4be82f18c090784584a911c83db838416
9000HAL <gsanchezscs@gmail.com>
2023-06-07 13:08:22 -0700

    hw ex #1 save

--- JS-day2-HW.js
+++ JS-day2-HW.js
@@ -1,4 +1,37 @@
-//==================Exercise #1 ==========//
+//==================Exercise #1 ============================================//
+
+// DEFINE function to display favorite food dishes
+function displayFavoriteDishes(person) {
+    // LOOP over properties of the object
+    for (let key in person) {
+        // CHECK of the property value is an array
+        if (Array.isArray(person[key])) {
+            // LOG each item in the array
+            console.log(`Favorite ${key}:`);
+            person[key].forEach(function(item) {
+                console.log(item);
+            });
+        } else {
+          // LOG the property value directly
+          console.log(`Favorite ${key}: ${person[key]}`);
+        }
+    } 
+
+}
+
+// DEFINE the person3 object
+
+
+
+
+
+    
+
+
+
+
+
+
 
 
   
