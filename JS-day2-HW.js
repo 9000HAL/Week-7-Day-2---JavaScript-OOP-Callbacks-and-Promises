34779edbbd4c6b91bd588b5be9027aba49f1cc13
9000HAL <gsanchezscs@gmail.com>
2023-06-07 14:39:47 -0700

    start CODEWARS problems HW

--- JS-day2-HW.js
+++ JS-day2-HW.js
@@ -1,6 +1,5 @@
 //==================Exercise #1 ============================================//
 
-
 /*
 Write a function that parses through the below object and displays all of their
 favorite food dishes as shown:
@@ -62,7 +61,7 @@ let person3 = {
 displayFavoriteDishes(person3);
 
   
-//CONSOLE OUTPUT
+//CONSOLE OUTPUT:
 /*
 Favorite pizza:
 Deep Dish
@@ -84,11 +83,75 @@ Favorite shakes:
 
 //============Exercise #2 ============//
 
+/*
+Write an Javascript Class for a Person that has:
+a name 
+and age, 
+has a printInfo method, 
+and also has a method that increments the persons age by 1 each time the method is called
+Create two people using the 'new' keyword, 
+and print both of their infos 
+and increment one persons age by 3 years
+Use an arrow function for both methods
+*/
+// Create our Person Class
+// Use an arrow to create the printInfo method
+// Create another arrow function for the addAge method that takes a single parameter adding to the age 
+
+
+// DEFINE the Person class
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    // ARROW function for the printInfo method
+    printInfo = () => {
+        console.log(`Name: ${this.name}`);
+        console.log(`Age: ${this.age}`);
+    };
+
+    // ARROW function for the addAge method
+    addAge = (years) => {
+        this.age += years;
+    };
+
+}
+
+//Create two Person objects
+const person1 = new Person('Gabe', 52);
+const person2 = new Person('Melody', 40);
+
+// PRINT the info for both persons
+person1.printInfo();
+person2.printInfo();
+
+// INCREMENT person1's age by 3 years
+person1.addAge(3);
+
+// PRINT info for person1 after incrementing age
+person1.printInfo();
+
+
+//CONSOLE OUTPUT:
+/*
+"Name: Gabe"
+"Age: 52"
+"Name: Melody"
+"Age: 40"
+"Name: Gabe"
+"Age: 55"
+*/
 
 
 
-//CONSOLE OUTPUT
-//
+
+// ============= Exercise #3 ============//
+/*
+    Using the Ergast API, fetch to 'http://ergast.com/api/f1/2008/5/driverStandings.json'
+    handle the promise with callbacks (.then(), .catch()) or async/await, and log the data.
+*/
 
 
 
