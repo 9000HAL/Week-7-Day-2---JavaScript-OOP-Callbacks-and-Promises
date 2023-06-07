15f982d79197ef2e12dc28b94b90b6a407d50a17
9000HAL <gsanchezscs@gmail.com>
2023-06-07 12:45:15 -0700

    fomat for HW

---
+++ JS-day2-HW.js
@@ -0,0 +1,47 @@
+//==================Exercise #1 ==========//
+
+
+  
+  
+//CONSOLE OUTPUT
+/*
+
+*/
+
+
+
+
+//============Exercise #2 ============//
+
+
+
+
+//CONSOLE OUTPUT
+//
+
+
+
+
+
+
+
+
+//==================CODEWARS Problem #1 ==========//
+
+// CODEWARS/PYTHON PROBLEM #1 LINK: 
+
+
+//Console Output:
+
+
+
+
+
+
+
+
+//==================CODEWARS Problem #2 ==========//
+
+// CODEWARS/PYTHON PROBLEM #2 LINK: 
+
+//Console Output:
